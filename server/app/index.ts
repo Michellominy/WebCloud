9d2f5cee25f1cca857e32fc2736927fd2a97b125
michel <Lominy.michel@gmail.com>
2023-08-12 21:17:54 -0400

    added flask app

--- server/app/index.ts
+++
@@ -1,7 +0,0 @@
-import 'module-alias/register';
-import 'reflect-metadata';
-import { Server } from './server';
-import { Container } from 'typedi';
-
-const server: Server = Container.get(Server);
-server.init();
